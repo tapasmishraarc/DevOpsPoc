036d0db2b3f6c74182e14475d5ed43d139372c87
tapas <mail.tapasranjanmishra@gmail.com>
2018-02-09 16:00:11 +0530

    Added test solution

---
+++ SiteCoreApp/SiteCoreAppTest/HomeControllerTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SiteCoreApp.Controllers;
+using System.Web.Mvc;
+
+namespace SiteCoreAppTest
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void IndexActionReturnView()
+        {
+            var controller = new HomeController();
+            var result = controller.Index() as ViewResult;
+            Assert.AreEqual("Index", result.ViewName);
+        }
+    }
+}
