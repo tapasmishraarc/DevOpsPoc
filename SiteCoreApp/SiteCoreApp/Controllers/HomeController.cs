87d511aec73d88e3e7bf40602fdbd70afd01ddad
tapas <mail.tapasranjanmishra@gmail.com>
2018-02-09 16:48:55 +0530

    Added Controller

---
+++ SiteCoreApp/SiteCoreApp/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SiteCoreApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/  test
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+      
+    }
+}
